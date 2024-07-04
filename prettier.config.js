22933045ddee5d5e3bec769f7fb526710a626b63
appjson <app.json@hotmail.com>
2024-07-04 15:50:09 +0800

    Init project till Chapter 5

---
+++ prettier.config.js
@@ -0,0 +1,18 @@
+const config = {
+    trailingComma: 'es5', // 不添加最后一项的逗号
+    arrowParens: 'avoid',
+    tabWidth: 2,
+    useTabs: false,
+    bracketSameLine: false,
+    semi: true,
+    printWidth: 100,
+    bracketSpacing: true,
+    endOfLine: 'lf',
+    jsxSingleQuote: false,
+    singleQuote: true,
+    htmlWhitespaceSensitivity: 'css',
+    quoteProps: 'consistent',
+    singleAttributePerLine: false,
+};
+
+module.exports = config;
